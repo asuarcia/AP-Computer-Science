63468750af55736dfe99966c146709ab513c1cbc
Suarez Aitor <AS9155@NEWTOWNPS.K12>
2026-09-04 11:53:56 -0400

    program 0

---
+++ First Project/BasicManipulations.java
@@ -0,0 +1,29 @@
+public class BasicManipulations {
+    public static void main(String[] args) {
+         
+	/*Task 1
+    System.out.print("A");
+    System.out.println("OneTwo");
+    Output: A ONeTwo
+    Task 2
+     System.out.print("B One");
+    System.out.println("Two");
+    Output: B One
+            Two
+    Task 3
+    System.out.print("C One \nTwo");
+  
+    Task 4
+    System.out.print("D ");
+    System.out.println("One");
+    System.out.println("Two");
+    System.out.println("ThreeFour");
+    */
+
+    System.out.println("")
+    
+
+
+
+    }
+}
