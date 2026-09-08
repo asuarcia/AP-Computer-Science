6fb1d703a9c2cf7d48a8047d7f31efd78ecaaa2c
Suarez Aitor <AS9155@NEWTOWNPS.K12>
2026-09-08 09:55:35 -0400

    Intro to Algos

--- First Project/BasicManipulations.java
+++ First Project/BasicManipulations.java
@@ -1,5 +1,7 @@
 public class BasicManipulations {
     public static void main(String[] args) {
+        System.out.println("Program");
+
 /*         
 Task 1
     System.out.print("A");
---
+++ First Project/ObjectsMethods.java
@@ -0,0 +1,23 @@
+public class ObjectsMethods {
+    public static void main(String [] args) {
+/* 
+        int math = 1;
+        int science = 6;
+        int art = 7;
+
+        System.out.print("I take math period: ");
+        System.out.println(math);
+        System.out.println("I take science period: " + science);
+*/
+
+        int popcorn= 3;
+        int mm= 4;
+        int nerdscluster= 2;
+
+        System.out.println("popcorn = " + popcorn);
+        System.out.println("mm = " + mm);
+        System.out.println("nerdscluster = " + nerdscluster);
+        int total = popcorn + mm + nerdscluster;
+        System.out.println("total = " + total);
+    }      
+}
