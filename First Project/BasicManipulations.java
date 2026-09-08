72dd2bbe29c094de039832b2b6aceccf1dc09ec6
Suarez Aitor <AS9155@NEWTOWNPS.K12>
2026-09-08 09:25:26 -0400

    Up to task 8 on Program 0

--- First Project/BasicManipulations.java
+++ First Project/BasicManipulations.java
@@ -1,29 +1,71 @@
 public class BasicManipulations {
     public static void main(String[] args) {
-         
-	/*Task 1
+/*         
+Task 1
     System.out.print("A");
     System.out.println("OneTwo");
-    Output: A ONeTwo
-    Task 2
+        Output: 
+    A OneTwo
+
+Task 2
      System.out.print("B One");
     System.out.println("Two");
-    Output: B One
-            Two
-    Task 3
-    System.out.print("C One \nTwo");
+        console window:
+    B One
+    Two 
+Task 3
+    System.out.print("C One \nTwo"); 
+        console window: 
+    C One 
+    Two
   
-    Task 4
+Task 4
     System.out.print("D ");
     System.out.println("One");
     System.out.println("Two");
     System.out.println("ThreeFour");
-    */
-
-    System.out.println("")
+        console window: 
+    D One
+    Two
+    ThreeFour
     
 
+Task 5
+    System.out.println("E One\nTwo");
+    System.out.println("Three\nFour");
+        console window:  
+    E One
+    Two
+    Three
+    Four
 
+Task 6
+    System.out.println("F One");
+    System.out.println("Two\nThree\nFour");
+        console window:
+    F One
+    Two
+    Three
+    Four 
+Task 7
+    System.out.println("G One\nTwo\nThree");
+    System.out.println("Four"); 
+    System.out.println("\"five\""); // The \"" Leaves the quotes in the output
+        console window:
+    G One
+    Two
+    Three
+    Four
+    "five" 
+
+    System.out.println("H One\nTwo\nThree\nFour\n\"Five\"\n\tsix");
+       console window:
+    H One
+    Two
+    Three
+    Four
+    "five"
+             six */
 
     }
 }
