05bc8d589c8e360bd178841bd07c3bca7256d848
asuarcia <asuarcia@gmail.com>
2026-09-11 22:36:13 -0400

    Homework 2

--- First Project/House.java
+++ First Project/House.java
@@ -10,9 +10,51 @@ public class House {
         System.out.println("\t /      \\");
         System.out.println("\t |       |");
         System.out.println("\t |    _  |");
-        System.out.println("\t |   ||  |");
-        System.out.println("\t |___||__|");
-        System.out.println("\t "Computer house"");
- 
+        System.out.println("\t |   | | |");
+        System.out.println("\t |___| |_|");
+        System.out.println("\t \"Computer House\"");
+ /*         console window
+            /\
+           /  \
+          /    \
+         /      \
+         |       |
+         |    _  |
+         |   | | |
+         |___| |_|
+         "Computer House" 
+ */
+
+    }
+}
+
+class Animal {
+    public static void main(String[] args) {
+        
+    System.out.println("\t    ____________  ");
+    System.out.println("\t   /   ______   \\");
+    System.out.println("\t  /   /   _  \\   \\ ");
+    System.out.println("\t  |   |  /  \\ |   |");
+    System.out.println("\t  |   | | 0  |    |");
+    System.out.println("\t  \\   | \\___/    / 0  0 ");
+    System.out.println("\t   \\  \\_______  /  / /");
+    System.out.println("\t  __\\__________/__/_/_");
+    System.out.println("\t |____________________|");
+    System.out.println("\t \"Computer Snail\"");
+/*      console window:
+            ____________  
+           /   ______   \
+          /   /   _  \   \ 
+          |   |  /  \ |   |
+          |   | | 0  |    |
+          \   | \___/    / 0  0 
+           \  \_______  /  / /
+          __\__________/__/_/_
+         |____________________|
+         "Computer Snail"
+*/
+
+
+        
     }
 }
