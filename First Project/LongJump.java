c7ed9b4eaf1903ff03aa189433f2b3b2a8b73cf3
Suarez Aitor <AS9155@NEWTOWNPS.K12>
2026-09-14 12:32:32 -0400

    Homework 3

---
+++ First Project/LongJump.java
@@ -0,0 +1,19 @@
+/* Aitor Suarez
+   Homework 3
+   Chp 1 */
+public class LongJump { //Declare class
+    public static void main(String[] args) { //Entry point of the program
+
+
+    System.out.println("The lengths of the jumps:\t'3.3 meters'\t'3.5 meters'\t'4.0 meters'\t'3.0 meters'"); //Stating the jumps as strings
+    System.out.println(""); // Empty line makes a space
+    System.out.println("The average jump is " + ((3.3  + 3.5 + 4.0 + 3.0) / 4) + " meters."); // The jumps are now doubles in order to do math on them
+
+
+    }
+} // Close off the class
+/* 
+        console window:
+    The lengths of the jumps:       '3.3 meters'    '3.5 meters'    '4.0 meters'    '3.0 meters'
+    The average jump is 3.45 meters.
+*/
